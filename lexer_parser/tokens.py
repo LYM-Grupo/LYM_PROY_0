99c03028f8ad71f2c33d7cb1f4e374bf49351656
agfarar <a.farfana@uniandes.edu.co>
2023-08-27 10:16:58 -0500

    modificaciones_1

--- lexer_parser/tokens.py
+++ lexer_parser/tokens.py
@@ -1,2 +1,7 @@
 #lexer_parser: Contiene archivos relacionados con el análisis léxico y sintáctico.
 #tokens.py: Define clases o estructuras para representar los tokens.
+
+def tokenizer(ruta_archivo):
+    with open(ruta_archivo, "r") as file:
+        for line in file:
+            print(line.strip())
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 #main.py: Punto de entrada del programa. Lee el archivo de programa de muestra y llama a los módulos relevantes para analizarlo.
+from lexer_parser import tokens as tok
+
+tok.tokenizer("/home/keith/Downloads/LYM_PROY_0/sample_program.txt")

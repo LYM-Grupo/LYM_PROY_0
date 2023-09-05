5b91c1217637e5a8c420c6c4b72530489e8225d9
fodepixo <arcilaangel12@gmail.com>
2023-09-05 11:53:33 -0500

    terminado if

--- lexer_parser/tokens.py
+++ lexer_parser/tokens.py
@@ -33,7 +33,7 @@ def verify_types(x,list_string_modified,defProc):#IMPORTANTE: Esto es mal toca c
     result=False
     for z in range(0,len(x)):
         if x[z]["args"] == 1:
-            #print(dict_procedures)#IMPORTANTE Esto es mal 
+            
             if x[z]["type_1"] == "value" and (list_string_modified[0].lower() in dict_variables.keys() or list_string_modified[0].lower() in dict_procedures[defProc] or list_string_modified[0].isdigit()):
                 result = True
             
@@ -517,7 +517,8 @@ def parse_definition_defProc(list_blocks,defProc):
                             return False
                     else:
                         return False
-                    #if string_modified[0].string.lower()=='' and lis
+                    
+
                 elif list_blocks[i+1].string.lower() == 'not' and list_blocks[i+2].string.lower() == ':':
 
                     if list_blocks[i+3].string.lower() == 'facing' and list_blocks[i+4].string.lower() == '(' and list_blocks[i+5].string.lower() in values_parameters['orientation'] and list_blocks[i+6].string.lower() == ')':
@@ -562,10 +563,13 @@ def parse_definition_defProc(list_blocks,defProc):
                                 return False
                         else:
                             return False
+                        
                     elif list_blocks[i+3].string.lower() == 'can':
-                        string_modified=string_block_code.replace('can','')[1:-1].strip()
+                        
+                        string_modified=string_block_code.replace('can','').replace('not','').replace(':','').strip().replace(' ','')[1:-1]
                         
                         list_tokens=list(tokenize.tokenize(BytesIO(string_modified.encode('utf-8')).readline))[1:-1]
+                        
                         list_string_modified= string_modified.replace(list_tokens[0].string.lower(),'')[1:-1].split(',')
                         
                         if list_string_modified[0] == '' and list_tokens[0].string.lower()=='nop':
@@ -575,6 +579,7 @@ def parse_definition_defProc(list_blocks,defProc):
                         if len(x) !=0:#tenemos que verificar que el numero de  argumentos en la funcion sea el correcto
                                 
                             if verify_types(x,list_string_modified,defProc):
+                                
                                 first_index=list_blocks[i].line.lower().find('{')
                                 second_index=list_blocks[i].line.lower().find('}')
                                 
@@ -594,15 +599,17 @@ def parse_definition_defProc(list_blocks,defProc):
                                         string_else_block=list_blocks[i].line[list_blocks[i].line.find("else")+4:].replace(' ','').strip()
                                         
                                         if string_else_block[-1]=='}' and string_else_block[0]=='{': #arreglar esto para los ;
+        
+                                            string_else_block=string_else_block[1:-1]
                                             list_tokens_3=list(tokenize.tokenize(BytesIO(string_else_block.encode('utf-8')).readline))[1:-1]
                                             list_string_modified_3 = string_else_block.replace(list_tokens_3[0].string.lower(),'')[1:-1].split(',')
                                             if list_string_modified_3[0]=='' and list_tokens_3[0].string.lower()== 'nop':#Aqui esta lo del nop IMPORTANTE 
                                                 z=[{"key":"nop","args":0,"type_1":"none"}]
                                             else:
                                                 z=search_list_dict_built_in_function(list_tokens_3[0].string.lower(),list_dict_built_in_function,len(list_string_modified_3))
-                                                
+                                            
                                             if len(z) !=0:
-                                                    
+                                                
                                                 if verify_types(z,list_string_modified_3,defProc):
                                                     i+=len(number_of_tokens)-1
                                                 else:
